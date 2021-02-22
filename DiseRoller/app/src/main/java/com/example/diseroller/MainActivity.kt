28ebbffcc5faeeaa3ba3f34b4fc68465cdc83f55
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-02-23 00:19:17 +0300

    Dice roll and dispalyed a random number

--- DiseRoller/app/src/main/java/com/example/diseroller/MainActivity.kt
+++ DiseRoller/app/src/main/java/com/example/diseroller/MainActivity.kt
@@ -7,15 +7,37 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
+
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton : Button = findViewById(R.id.buttonRoll)
-        val rollNumber: TextView = findViewById(R.id.textViewRoll)
+     //  val rollNumber: TextView = findViewById(R.id.textViewRoll)
 
         rollButton.setOnClickListener {
-            Toast.makeText(this,"Dice Roled",Toast.LENGTH_LONG).show()
+            Toast.makeText(this,"Dice Rolled ",Toast.LENGTH_LONG).show()
+             rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val rollNumber: TextView = findViewById(R.id.textViewRoll)
+        rollNumber.text = diceRoll.toString()
+
+
+    }
+
+
+}
+class Dice(private val  numSides:Int){
+
+    fun roll():Int{
+        return (1..numSides).random()
     }
 }
