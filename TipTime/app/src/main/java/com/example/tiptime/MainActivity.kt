8bfd63a47002ce8cf3bc8a6a7e36bb98ee4af5cb
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-02-26 02:57:22 +0300

    hide keyboard on edit text

--- TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tiptime
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tiptime.databinding.ActivityMainBinding
@@ -32,6 +36,9 @@ class MainActivity : AppCompatActivity() {
 
         binding.calculateButton.setOnClickListener {
             calculateTip()
+        }
+
+        binding.CostServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)
         }
     }
 
@@ -81,5 +88,16 @@ class MainActivity : AppCompatActivity() {
         NumberFormat.getCurrencyInstance()
         val formatedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formatedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
